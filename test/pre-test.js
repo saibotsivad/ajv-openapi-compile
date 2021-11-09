1480b2d2ee65458df45b8ff95ebc8b28a1a5dedc
Tobias Davis <tobias@davistobias.com>
2021-11-09 10:38:06 -0600

    change: build definition.json from the definition.js for single canonical

--- test/pre-test.js
+++ test/pre-test.js
@@ -4,4 +4,5 @@ import { definition } from './definition.js'
 
 const codeString = compile(definition)
 
+writeFileSync('./test/definition.json', JSON.stringify(definition, undefined, 4), 'utf8')
 writeFileSync('./test/compiled.cjs', codeString, 'utf8')
